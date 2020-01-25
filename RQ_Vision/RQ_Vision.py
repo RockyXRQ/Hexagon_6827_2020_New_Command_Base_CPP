1edbcceff017112b7c17d2b5338018d100f3221e
RockyXRQ <1315930212@qq.com>
2020-01-25 22:02:16 +0800

    Speed up & add networkTables to deliver data.

--- RQ_Vision/RQ_Vision.py
+++ RQ_Vision/RQ_Vision.py
@@ -1,29 +1,30 @@
 from cv2 import cv2
 from HSV_Base_Grip import GripPipeline
+from networktables import NetworkTablesInstance
 
 # TODO1: Add new thread.
-# TODO2: Add NetworkTable to deliver data.
 
 def extra_processing(showFrame, pipeline):
-    center_x_positions = []
-    center_y_positions = []
-    widths = []
-    heights = []
+    m_table = NetworkTablesInstance.getDefault().getTable("JETSONNANO")
 
-    # Find the bounding boxes of the contours to get x, y, width, and height
-    for contour in pipeline.filter_contours_output:
-        x, y, w, h = cv2.boundingRect(contour)
-        # X and Y are coordinates of the top-left corner of the bounding box
-        center_x_positions.append(x + w / 2)
-        center_y_positions.append(y + h / 2)
-        widths.append(w)
-        heights.append(y)
-        cv2.drawContours(showFrame, contour, -1, 255, 3)
+    # Get the biggest contor.
+    if pipeline.filter_contours_output:
+        m_MAXCnt = sorted(pipeline.filter_contours_output,
+                          key=cv2.contourArea)[0]
+        # Find the bounding boxes of the contour to get x, y, width, and height
+        x, y, w, h = cv2.boundingRect(m_MAXCnt)
+        m_table.putBoolean("isHaveTarget", True)
+        m_table.putNumber("SpinError", (x + w / 2) - showFrame.shape[1])
+        cv2.drawContours(showFrame, m_MAXCnt, -1, 255, 3)
         cv2.circle(
-            showFrame, (int(center_x_positions[0]), int(center_y_positions[0])), 3, (0, 0, 255), -1)
-        cv2.imshow("match?", showFrame)
-        cv2.waitKey(10)
-    print("X:", center_x_positions, "Y", center_y_positions)
+            showFrame, (int(x + w / 2), int(y + h / 2)), 3, (0, 0, 255), -1)
+        print("Target Get!!! ","X:", int(x + w / 2), "Y:", int(y + h / 2))
+    else:
+        print("Target Lose QvQ")
+        m_table.putBoolean("isHaveTarget", False)
+
+    cv2.imshow("match?", showFrame)
+    cv2.waitKey(1)
 
 
 def main():
@@ -40,7 +41,6 @@ def main():
             extra_processing(frame, pipeline)
 
     print('Capture closed')
-
 
 if __name__ == '__main__':
     main()
